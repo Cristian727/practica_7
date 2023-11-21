470f83eeb4ccfb22e2a73b0b412a88faf7edeed2
Cristian727 <cristian.gonzalez7@hotmail.com>
2023-11-21 14:59:44 +0100

    css casi terminado

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function eventoHTMLButton(){
     document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con evento HTML"
+    document.querySelector("#textoResultado").style.background = "red"
 }
 
 const boton2 = document.querySelector("#boton2")
 
 function eventoListener(){
     document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con addEventListener"
+    document.querySelector("#textoResultado").style.background = "blue"
 
 }
 
 
 boton2.addEventListener("click", () =>{
     document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con addEventListener"
+    document.querySelector("#textoResultado").style.background = "blue"
+
 })
 
 //boton2.addEventListener("click", eventoListener)
@@ -20,4 +24,6 @@ const boton3 = document.querySelector("#boton3")
 
 boton3.onclick = () => {
     document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con event handler"
+    document.querySelector("#textoResultado").style.background = "yellow"
+
 }
