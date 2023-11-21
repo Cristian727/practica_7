a57ee7a0ab584412b43a4e042b448434c9c877a0
Cristian727 <cristian.gonzalez7@hotmail.com>
2023-11-21 14:32:03 +0100

    js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function eventoHTMLButton(){
     document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con evento HTML"
 }
+
+const boton2 = document.querySelector("#boton2")
+
+function eventoListener(){
+    document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con addEventListener"
+
+}
+
+
+boton2.addEventListener("click", () =>{
+    document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con addEventListener"
+})
+
+//boton2.addEventListener("click", eventoListener)
+
+const boton3 = document.querySelector("#boton3")
+
+boton3.onclick = () => {
+    document.querySelector("#textoResultado").innerHTML = "Has hecho click en el botón con event handler"
+}
